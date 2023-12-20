ed3326e2a41b8a7c44bb7f737b7e92a5616c80a2
Yasser Shalash <yshalash@f0r6s2.codam.nl>
2023-12-20 12:37:27 +0100

    need to fix 0 as input

--- ft_printf.h
+++ ft_printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yasser <marvin@42.fr>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/13 20:38:20 by yasser            #+#    #+#             */
-/*   Updated: 2023/11/15 15:04:27 by yasser           ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_printf.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: yasser <marvin@42.fr>                        +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/13 20:38:20 by yasser        #+#    #+#                 */
+/*   Updated: 2023/12/20 12:14:24 by yshalash      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+int	ft_printf(const char *str, ...);
 int	ft_putchar(char c);
 int	ft_put_downcase_hex(unsigned int num);
 int	ft_putnbr(int num);
